33ea4621db05ebd7d3029ca01359c186c88aba0d
JeronimoCachafeiro <jeronimocachafeiro@gmail.com>
2022-10-03 07:30:12 -0300

    Add player win conditions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 function getComputerChoice() {
-  let possibleShapes = ['rock', 'paper', 'scissors']
+  let possibleShapes = ["rock", "paper", "scissors"];
   let chosenShape = possibleShapes[Math.floor(Math.random() * possibleShapes.length)];
-  return chosenShape
+  console.log(chosenShape);
+  return chosenShape;
 }
 
 function getPlayerChoice() {
-  let playerChoice = prompt('Choose a shape: ').toLowerCase();
+  let playerChoice = prompt("Choose a shape: ").toLowerCase();
   return playerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    
-}
+function playRound(computerSelection, playerSelection) {
+  // Player wins
+  if (playerSelection === "rock" && computerSelection == "scissors") {
+    return "You Win! Rock beats Scissors!";
+  } else if (playerSelection === "paper" && computerSelection == "rock") {
+    return "You Win! Paper beats Rock!";
+  } else if (playerSelection === "scissors" && computerSelection == "paper") {
+    return "You Win! Scissors beat Paper!";  
+  } 
 
+}
