7711d2c366b5fb0435c0496bf25c3c2053fa7f4f
JeronimoCachafeiro <jeronimocachafeiro@gmail.com>
2022-10-03 10:14:19 -0300

    Add player lose conditions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function getComputerChoice() {
   let possibleShapes = ["rock", "paper", "scissors"];
-  let chosenShape = possibleShapes[Math.floor(Math.random() * possibleShapes.length)];
+  let chosenShape =
+    possibleShapes[Math.floor(Math.random() * possibleShapes.length)];
   console.log(chosenShape);
   return chosenShape;
 }
@@ -11,13 +12,19 @@ function getPlayerChoice() {
 }
 
 function playRound(computerSelection, playerSelection) {
-  // Player wins
   if (playerSelection === "rock" && computerSelection == "scissors") {
     return "You Win! Rock beats Scissors!";
   } else if (playerSelection === "paper" && computerSelection == "rock") {
     return "You Win! Paper beats Rock!";
   } else if (playerSelection === "scissors" && computerSelection == "paper") {
-    return "You Win! Scissors beat Paper!";  
-  } 
-
+    return "You Win! Scissors beat Paper!";
+  } else if (playerSelection === "rock" && computerSelection == "paper") {
+    return "You Lose! Paper beats Rock!";
+  } else if (playerSelection === "paper" && computerSelection == "scissors") {
+    return "You Lose! Scissors beat Paper!";
+  } else if (playerSelection === "scissors" && computerSelection == "rock") {
+    return "You Lose! Rock beats Scissors!";
+  }
 }
+
+
