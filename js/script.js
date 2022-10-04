11e3145a0ed1d2f723724a29b0fae68895d38f3b
JeronimoCachafeiro <jeronimocachafeiro@gmail.com>
2022-10-04 13:29:35 -0300

    Add UI

--- js/script.js
+++ js/script.js
@@ -2,13 +2,9 @@ function getComputerChoice() {
   let possibleShapes = ["rock", "paper", "scissors"];
   let chosenShape =
     possibleShapes[Math.floor(Math.random() * possibleShapes.length)];
-  console.log(chosenShape);
+  uiComputerChoice.textContent = `Computer chose: ${chosenShape}`;
+  results.appendChild(uiComputerChoice);
   return chosenShape;
-}
-
-function getPlayerChoice() {
-  let playerChoice = prompt("Choose a shape: ").toLowerCase();
-  return playerChoice;
 }
 
 function toTitle(str) {
@@ -21,35 +17,59 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "paper" && computerSelection == "rock") ||
     (playerSelection === "scissors" && computerSelection == "paper")
   ) {
-    console.log(
-      `You Win! ${toTitle(playerSelection)} beats ${toTitle(
-        computerSelection
-      )}!`
-    );
-    return 1;
+    uiRoundOutcome.textContent = `You Win! ${toTitle(
+      playerSelection
+    )} beats ${toTitle(computerSelection)}!`;
+    results.appendChild(uiRoundOutcome);
+    ++score;
+    uiScore.textContent = `Score: ${score}`;
   } else if (
     (playerSelection === "rock" && computerSelection == "paper") ||
     (playerSelection === "paper" && computerSelection == "scissors") ||
     (playerSelection === "scissors" && computerSelection == "rock")
   ) {
-    console.log(
-      `You Lose! ${toTitle(computerSelection)} beats ${toTitle(
-        playerSelection
-      )}!`
-    );
-    return 0;
+    uiRoundOutcome.textContent = `You Lose! ${toTitle(
+      computerSelection
+    )} beats ${toTitle(playerSelection)}!`;
+    results.appendChild(uiRoundOutcome);
+    uiScore.textContent = `Score: ${score}`;
   } else {
-    console.log("It's a tie!");
-    return 0;
+    uiRoundOutcome.textContent = "It's a tie!";
+    results.appendChild(uiRoundOutcome);
+    uiScore.textContent = `Score: ${score}`;
   }
 }
 
-function game() {
-  let score = 0;
-  for (let i = 0; i < 5; i++) {
-    score += playRound(getPlayerChoice(), getComputerChoice(), score);
-  }
-  console.log(`Final score: ${score}`);
-}
+const btns = document.querySelectorAll(".btn");
 
-game();
+const results = document.querySelector(".results");
+const uiPlayerSelection = document.createElement("p");
+const uiComputerChoice = document.createElement("p");
+const uiRoundOutcome = document.createElement("p");
+const uiScore = document.createElement("p");
+const gameResult = document.createElement("p");
+
+let score = 0;
+let count = 1;
+for (const btn of btns) {
+  btn.addEventListener("click", function () {
+    if (count < 5) {
+      count++;
+      console.log(count);
+      console.log(score);
+      const playerSelection = this.textContent.toLowerCase();
+      uiPlayerSelection.textContent = `You chose: ${playerSelection}`;
+      results.appendChild(uiPlayerSelection);
+      const computerSelection = getComputerChoice();
+      playRound(playerSelection, computerSelection);
+      results.appendChild(uiScore);
+    } else {
+      if (score >= 3) {
+        gameResult.textContent = "Congratulations! You win!";
+      } else {
+        gameResult.textContent = "Oh no! The computer wins!";
+      }
+      results.appendChild(gameResult);
+    }
+  });
+}
